2b9f79fb4eee5f2704140c2b63886f0c2ff12277
EC2 Default User <ec2-user@ip-172-31-44-100.eu-west-1.compute.internal>
2020-01-27 13:14:29 +0000

    a

--- anomalies/index.php
+++ anomalies/index.php
@@ -2,7 +2,14 @@
 $handle = popen("tail -f /tmp/data.txt 2>&1", 'r');
 while(!feof($handle)) {
     $buffer = fgets($handle);
-    echo "$buffer ala<br/>\n";
+    #echo "$buffer ala<br/>\n";
+    #echo "<h2 style='color: blue'>";
+    $color = 90;
+    #echo "<p style=\"color: hsl(0, $color%, 50%)\">";
+    echo "<p style=\"background-color: hsl(0, $color%, 50%)\">";
+    #echo "<h1 bgcolor='##386de8'>";
+    echo "$buffer <br/>\n";
+#    echo "</p>";
     ob_flush();
     flush();
 }
