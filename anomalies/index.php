6c52d8b812dc4ebfa4024a97c4339af332c4aa08
EC2 Default User <ec2-user@ip-172-31-44-100.eu-west-1.compute.internal>
2020-01-27 13:32:25 +0000

    a

--- anomalies/index.php
+++ anomalies/index.php
@@ -1,16 +1,33 @@
 <?php
 $handle = popen("tail -f /tmp/data.txt 2>&1", 'r');
+
+#echo "<table border=0 >";
+echo "<table width=100% border=0 cellpadding=0 cellspacing=0 >";
 while(!feof($handle)) {
-    $buffer = fgets($handle);
-    #echo "$buffer ala<br/>\n";
-    #echo "<h2 style='color: blue'>";
-    $color = 90;
-    #echo "<p style=\"color: hsl(0, $color%, 50%)\">";
+    $buffer1 = (float) fgets($handle);
+    $buffer2 = $buffer1 + 0.5;
+    $buffer3 = $buffer1 + 0.8;
+  
+
+    echo "<tr>"; 
+    echo "<td>";
+    $color = $buffer1 * 50; 
     echo "<p style=\"background-color: hsl(0, $color%, 50%)\">";
-    #echo "<h1 bgcolor='##386de8'>";
-    echo "$buffer <br/>\n";
-#    echo "</p>";
+    echo "$buffer1";
+    echo "</td><td>";
+    $color = $buffer2 * 30; 
+    echo "<p style=\"background-color: hsl(0, $color%, 50%)\">";
+    echo "$buffer2";
+    echo "</td><td>";
+    $color = $buffer3 * 40; 
+    echo "<p style=\"background-color: hsl(0, $color%, 50%)\">";
+    echo "$buffer3";
+
+
+    echo "</td>";
+    echo "</tr>"; 
     ob_flush();
     flush();
 }
+echo "</table>";
 pclose($handle);
