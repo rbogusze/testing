9b812e42376fe8df750fd6a20dd5727ca7b183ca
remi <you@example.com>
2020-01-29 14:54:22 +0000

    a

--- anomalies/analytics10.sql
+++ anomalies/analytics10.sql
@@ -5,18 +5,32 @@ CREATE OR REPLACE STREAM "TEMP_STREAM" (
    "column1"        DOUBLE,
    "column2"        DOUBLE,
    "column3"        DOUBLE,
+   "column4"        DOUBLE,
+   "column5"        DOUBLE,
+   "column6"        DOUBLE,
+   "column7"        DOUBLE,
+   "column8"        DOUBLE,
+   "column9"        DOUBLE,
+   "column10"        DOUBLE,
    "ANOMALY_SCORE"  DOUBLE);
 -- Creates an output stream and defines a schema
 CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM" (
    "column1"        DOUBLE,
    "column2"        DOUBLE,
    "column3"        DOUBLE,
+   "column4"        DOUBLE,
+   "column5"        DOUBLE,
+   "column6"        DOUBLE,
+   "column7"        DOUBLE,
+   "column8"        DOUBLE,
+   "column9"        DOUBLE,
+   "column10"        DOUBLE,
    "ANOMALY_SCORE"  DOUBLE);
  
 -- Compute an anomaly score for each record in the source stream
 -- using Random Cut Forest
 CREATE OR REPLACE PUMP "STREAM_PUMP" AS INSERT INTO "TEMP_STREAM"
-SELECT STREAM "COL1", "COL2", "COL3", "ANOMALY_SCORE" FROM
+SELECT STREAM "COL1", "COL2", "COL3", "COL4", "COL5", "COL6", "COL7", "COL8", "COL9", "COL10", "ANOMALY_SCORE" FROM
   TABLE(RANDOM_CUT_FOREST(
     CURSOR(SELECT STREAM * FROM "SOURCE_SQL_STREAM_001")
   )
