5c13edafb065331e3d9dfb954b289c106d48b012
EC2 Default User <ec2-user@ip-172-31-36-42.eu-west-1.compute.internal>
2020-01-29 11:38:02 +0000

    a

--- anomalies/python_producer.py
+++ anomalies/python_producer.py
@@ -1,1 +1,26 @@
-print "ala ma kotaya"
+from kinesis_producer import KinesisProducer
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger('logger')
+logger.warning('this is a log message')
+
+config = dict(
+    aws_region='eu-west-1',
+    buffer_size_limit=100000,
+    buffer_time_limit=0.2,
+    kinesis_concurrency=1,
+    kinesis_max_retries=10,
+    record_delimiter='\n',
+    stream_name='remi_data_stream',
+    )
+
+k = KinesisProducer(config=config)
+
+records = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+
+for record in records:
+    k.send(record)
+
+k.close()
+k.join()
