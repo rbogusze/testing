e4ad7b3ab12a5fef9dbd1cbc2701de22ea8243be
remi <you@example.com>
2020-01-30 11:39:41 +0000

    a

--- anomalies/python_consumer.py
+++ anomalies/python_consumer.py
@@ -5,6 +5,7 @@ logging.basicConfig()
 logger = logging.getLogger('logger')
 logger.warning('this is a log message')
 
-consumer = KinesisConsumer(stream_name='remi_data_stream')
+#consumer = KinesisConsumer(stream_name='remi_data_stream')
+consumer = KinesisConsumer(stream_name='remi_data_anomalies')
 for message in consumer:
     print "Received message: {0}".format(message)
--- anomalies/stringc_generator.py
+++ anomalies/stringc_generator.py
@@ -1,10 +1,16 @@
 
 count = 0
-max_count = 10
+max_count = 100
+
+one_line = ""
+
 while True: 
     if count > max_count: 
         break
 
     print("   \"column{}\"        DOUBLE,".format(count)) 
     count += 1 
+    one_line = one_line + "\"COL" + str(count) + "\", "
+
+print (one_line)
   
