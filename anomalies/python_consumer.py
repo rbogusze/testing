87cc922d77b1107df7f99d4cb3c5f82054bb9a4b
remi <you@example.com>
2020-01-30 22:48:53 +0000

    a

--- anomalies/python_consumer.py
+++ anomalies/python_consumer.py
@@ -6,6 +6,7 @@ logger = logging.getLogger('logger')
 logger.warning('this is a log message')
 
 #consumer = KinesisConsumer(stream_name='remi_data_stream')
-consumer = KinesisConsumer(stream_name='remi_data_anomalies')
+#consumer = KinesisConsumer(stream_name='remi_data_anomalies')
+consumer = KinesisConsumer(stream_name='anomaly_output_stream')
 for message in consumer:
     print "Received message: {0}".format(message)
