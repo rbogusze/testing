433d0669ba2ec8d781a4e842944d5ff8ace482fc
remi <you@example.com>
2020-01-30 12:13:58 +0000

    create stream

---
+++ anomalies/python_producer_create_streams.py
@@ -0,0 +1,88 @@
+from kinesis_producer import KinesisProducer
+
+import time
+import os
+import logging
+logging.basicConfig()
+logger = logging.getLogger('logger')
+logger.warning('this is a log message')
+
+#-------------------
+# First I need to be able to create a Kinesis Data Stream from API
+
+def create_kinesis_data_stream(stream_name):
+  print(stream_name)
+  os.system("aws kinesis create-stream --stream-name " + stream_name + " --shard-count 1 --region eu-west-1")
+  os.system("aws kinesis add-tags-to-stream --stream-name " + stream_name + " --tags Responsible=Sokalszczuk,Project=SPM,Interesting=how_much_it_costs")
+
+
+
+create_kinesis_data_stream("ala_ma_kota")
+
+exit()
+#------------------
+
+config = dict(
+    aws_region='eu-west-1',
+    buffer_size_limit=100000,
+    buffer_time_limit=0.2,
+    kinesis_concurrency=1,
+    kinesis_max_retries=10,
+    record_delimiter='\n',
+    stream_name='remi_data_stream',
+    )
+
+k = KinesisProducer(config=config)
+
+# read from array
+#records = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+#for record in records:
+#    k.send(record)
+
+# read from file
+file1 = open('/home/ec2-user/result.csv', 'r') 
+count = 0
+
+while True: 
+    count += 1
+  
+    # Get next line from file 
+    line = file1.readline() 
+
+    # replace comma ',' with space ' '
+    # line = line.replace(",", " ")
+
+    # looks like there are too many columns, I need to be able to select only handful of them
+    line_split = line.split(",")
+
+    columns_interesting = 100
+    wcount = 0
+    small_line = ""
+    while wcount < columns_interesting:
+        small_line = small_line + "," + line_split[wcount]
+        wcount += 1
+
+    # remove first comma
+    small_line = small_line[1:]
+    #print(small_line)
+    
+    # send it to Kinesis
+    #k.send(line)
+    k.send(small_line)
+
+    time.sleep(1)
+  
+    # if line is empty 
+    # end of file is reached 
+    if not line: 
+        break
+    #print("Line{}: {}".format(count, line.strip())) 
+    #print("Line{}: {}".format(count, line)) 
+    print("Line{}: {}".format(count, small_line)) 
+    print("Sending line {}".format(count)) 
+    #break
+  
+file1.close() 
+
+k.close()
+k.join()
