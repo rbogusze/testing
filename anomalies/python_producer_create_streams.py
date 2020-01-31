0e917ceb9069c62c6edebd355936641b8f0a1379
remi <you@example.com>
2020-01-31 12:41:08 +0000

    a

--- anomalies/python_producer_create_streams.py
+++ anomalies/python_producer_create_streams.py
@@ -5,19 +5,42 @@ import os
 import logging
 logging.basicConfig()
 logger = logging.getLogger('logger')
-logger.warning('this is a log message')
+#logger.warning('this is a log message')
+
+# global variables
+nr_streams = 3
 
 #-------------------
 # First I need to be able to create a Kinesis Data Stream from API
 
 def create_kinesis_data_stream(stream_name):
-  print(stream_name)
-  os.system("aws kinesis create-stream --stream-name " + stream_name + " --shard-count 1 --region eu-west-1")
-  os.system("aws kinesis add-tags-to-stream --stream-name " + stream_name + " --tags Responsible=Sokalszczuk,Project=SPM,Interesting=how_much_it_costs")
+    print(stream_name)
+    os.system("aws kinesis create-stream --stream-name " + stream_name + " --shard-count 1 --region eu-west-1")
+    os.system("aws kinesis add-tags-to-stream --stream-name " + stream_name + " --tags Responsible=Sokalszczuk,Project=SPM,Interesting=how_much_it_costs")
+
+def create_kinesis_analytics(analytics_name, stream_name):
+    print(stream_name)
+    # prepare template
+    os.system("cat create-application.json | sed -e 's/ala_ma_kota/" + stream_name + "/' | sed -e 's/cadabra1/"+ analytics_name + "/' > trash_template.json")
+    os.system("aws kinesisanalytics create-application --cli-input-json file://trash_template.json")
+    
+
+     
+
+# declare a list that will store streams
+nr_stream = 0
+name_stream = ""
+for nr_stream in range(0,nr_streams):
+    name_stream = ("anomaly_input_stream" + str(nr_stream))
+    name_analytics = ("anomaly_analytics" + str(nr_stream))
+    print("Creating Kinesis stream: {}".format(name_stream))
+    create_kinesis_data_stream(name_stream)
+    print("Creating Kinesis analytics for: {}".format(name_analytics))
+    create_kinesis_analytics(name_analytics, name_stream)
+    exit() 
 
 
-
-create_kinesis_data_stream("ala_ma_kota")
+print("Creating Kinesis stream: anomaly_output_stream")
 create_kinesis_data_stream("anomaly_output_stream")
 
 exit()
