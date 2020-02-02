b138411f7938878014178b650553e084c7abe989
remi <you@example.com>
2020-02-02 13:30:04 +0000

    a

--- anomalies/python_consumer_cvs.py
+++ anomalies/python_consumer_cvs.py
@@ -9,6 +9,9 @@ logger = logging.getLogger('logger')
 
 the_file = open("/tmp/data_from_stream.txt", "a+")
 
+nr_columns_in_row = 3
+nr_counter = 1
+
 consumer = KinesisConsumer(stream_name=str(sys.argv[1]))
 for message in consumer:
     print "Received message: {0}".format(message)
@@ -16,7 +19,13 @@ for message in consumer:
     print "--- this is a dict, so I should be able to access it like message[\"Data\"]"
     print(message["Data"])
     print "Writing to file /tmp/data_from_stream.txt"
-    the_file.write(message["Data"] + '\n')
+    if nr_counter < nr_columns_in_row:
+        the_file.write(message["Data"].rstrip() + ',')
+        nr_counter += 1
+    else:
+        the_file.write(message["Data"].rstrip() + '\n')
+        nr_counter = 1
+        the_file.flush()
 
 
 the_file.close()
