a0b14ee9cb2f781afb79bcb32d0bcc194aff6119
remi <you@example.com>
2020-02-02 13:14:44 +0000

    yep

--- anomalies/python_consumer_cvs.py
+++ anomalies/python_consumer_cvs.py
@@ -2,28 +2,21 @@ from kinesis.consumer import KinesisConsumer
 
 import sys
 import logging
-import json
-import re
 
 logging.basicConfig()
 logger = logging.getLogger('logger')
 #logger.warning('this is a log message')
 
+the_file = open("/tmp/data_from_stream.txt", "a+")
+
 consumer = KinesisConsumer(stream_name=str(sys.argv[1]))
 for message in consumer:
     print "Received message: {0}".format(message)
-    # filter out the datetime.datetime
-    print(message)
-    message_re = re.sub('u.*tzinfo\=tzlocal\(\)\)\,', '', str(message))
-    print("-------")
-    print(message_re)
-    
-    json_string = json.dumps(message_re)
-    print(json_string)
-    #jdata = json.loads(json_string)
-    jdata = json.loads(message_re)
-    print(jdata)
-    print("-------omg")
-    print(jdata["Data"])
+    print(type(message))
+    print "--- this is a dict, so I should be able to access it like message[\"Data\"]"
+    print(message["Data"])
+    print "Writing to file /tmp/data_from_stream.txt"
+    the_file.write(message["Data"] + '\n')
 
 
+the_file.close()
