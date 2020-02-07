0ca640c87f70057019f860141344d8be0ad5fc80
remi <you@example.com>
2020-02-07 13:07:22 +0000

    a

--- anomalies/python_consumer.py
+++ anomalies/python_consumer.py
@@ -14,5 +14,15 @@ consumer = KinesisConsumer(stream_name=str(sys.argv[1]))
 
 print(dir(consumer))
 
+message_count = 1
 for message in consumer:
-    print "Received message: {0}".format(message)
+    #print "Received message: {0}".format(message)
+    #print (message["Data"])
+    string = message["Data"]
+    lst = string.split(",")
+    stringcount = len(lst)
+    commacount = string.count(',')
+    endlinecount = string.count('\n')
+    print "Message: {0}, there are: {1} elements in one message; comma count: {2} endline count: {3}".format(message_count,stringcount,commacount,endlinecount)
+    message_count += 1 
+
