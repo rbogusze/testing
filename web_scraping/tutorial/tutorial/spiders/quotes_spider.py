35cbb639c1b7ac23afb0c1c4bfec315f1b2afd61
Ubuntu <ubuntu@ip-172-31-39-71.eu-west-1.compute.internal>
2020-02-18 15:16:14 +0000

    a

--- web_scraping/tutorial/tutorial/spiders/quotes_spider.py
+++ web_scraping/tutorial/tutorial/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import datetime
 
 
 class QuotesSpider(scrapy.Spider):
@@ -9,8 +10,24 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+
+        # here I can do everything that I can do in interactive mode
+        title = response.css('title::text').get()
+        date = datetime.date.today()
+
+        print('Hello--------------')
+        #print(dir(response.request))
+        #print(response.request.headers)
+        print(date)
+        print('/Hello--------------')
+
+        # he is looping here through div.quote class, as visible in html source code, that is why title is not here
         for quote in response.css('div.quote'):
+            #print (dir(quote))
             yield {
+                'date': date,
+                'url': response.request.url,
+                'title': title,
                 'text': quote.css('span.text::text').get(),
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
