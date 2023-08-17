3548b290095731072f6faa8c475a942c65ce6365
remi <remik@example.com>
2023-08-17 13:06:01 +0200

    a

--- checking.sh
+++ checking.sh
@@ -12,8 +12,8 @@ echo "[debug] PROC_WITH_DEBUG_COUNT: ${PROC_WITH_DEBUG_COUNT}"
 if [ "${PROC_WITH_DEBUG_COUNT}" -gt 0 ]; then
   echo "Found processes running with debug enabled: \n ${PROC_WITH_DEBUG}"
   
-  echo "Exiting with 1 as this needs to be investigated"
+  echo "Exiting with 1 as this needs to be investigated."
   exit 1
 else
-  echo "No processes found that are running with debug enabled"
+  echo "No processes found that are running with debug enabled."
 fi
